92b3a1554b668f0a8a9477bd907ec59b3d083c37
Andys-in <andyjeffersonperez@gmail.com>
2023-03-16 22:03:39 -0400

    start of proyect

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your Code Here
+function start() {
+    let answer = window.prompt('Do you head left, or right?')
+    console.log(answer)
+    if (answer ==='left') {
+        answerNo1()
+    }else if (answer === 'right') {
+        answerNo2()
+    }
+
+}
+
+
+
+function answerNo1(){
+    let firstAnswer = window.prompt('Dou you like regueeton. Yes or No?')
+    console.log(firstAnswer)
+    if (firstAnswer === 'Yes'){
+        let yess = window.prompt('you suck')
+    }else if (firstAnswer === 'No'){
+        let naaa = window.prompt('you rock')
+    }
+}
+
+
+
+
+function answerNo2() {
+    let secondAnswer = window.prompt('Would you rather eat Pizza or Burger? ')
+console.log(secondAnswer)
+if (secondAnswer === 'Pizza'){
+    let xdxd = window.prompt('nomo')
+}else if (secondAnswer === 'Burger or burger'){
+    let third = window.prompt('Mind')
+}
+}
+
+
+
+
+start()
